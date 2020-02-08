66279bea2125615aeb82d0acbe0d40ec111a2e0f
Vinit Suryarao <60574327+VinitSuryarao@users.noreply.github.com>
2020-02-08 18:06:32 +0530

    Add files via upload

---
+++ Async_Await/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async_Await
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Action
+            Task.Run(async () => {
+
+                // Async n Await same as Multithreading, But Multithreading cannot pass paramter from one thread to another
+                // Async n Await can pass paramter to one thread to another.
+                // Async Assign function to Task
+                // Task run Function on new thread
+                // Await pass the parameter between threads
+
+                AsyncDemo obj = new AsyncDemo();
+                int result = await obj.Function1(); 
+                result = await obj.Function2(result); 
+                result = await obj.Function3(result); 
+
+                Console.WriteLine(result);
+                Console.ReadLine();
+
+
+
+            }).Wait();
+        }
+    }
+
+    public class AsyncDemo
+    {
+        // Runs on Thread T1
+        public async Task<int> Function1()
+        {
+            // Await pass the parameters from one thread to another
+            return await Task.Run(() =>
+            {
+
+                return 10;
+
+            });
+        }
+
+        // Runs on Thread T2
+        public async Task<int> Function2(int func1value)
+        {
+            // Await pass the parameters from one thread to another
+            return await Task.Run(() =>
+            {
+
+                return func1value + 10;
+
+            });
+        }
+
+        // Runs on Thread T3
+        public async Task<int> Function3( int func2value)
+        {
+            // Await pass the parameters from one thread to another
+            return await Task.Run(() =>
+            {
+
+                return func2value+ 10;
+
+            });
+        }
+    }
+}
